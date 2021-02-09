2b55f8daf6e81bdff9990c16dd54435934d70ea3
Guoqq17 <guoqq77@gmail.com>
2021-02-08 21:36:16 -0800

    floyd

--- HW2/hw2.m
+++ HW2/hw2.m
@@ -24,7 +24,7 @@ num_gabor = 100;                            % number of the time points to take
 t_gabor = linspace(0, t(end), num_gabor);   % discretize the time
 s_gabor = zeros(length(t_gabor), n);        % matrix to store the Gabor transforms
 
-%% tune wt
+% tune wt
 figure(2)
 for w_t = 1:1:length(width_t_all)
     for i=1:length(t_gabor)
@@ -41,7 +41,8 @@ for w_t = 1:1:length(width_t_all)
     axis([0, tr_gnr, 0, 1000])
     title(['wt = ', num2str(width_t_all(w_t))])
 end
-%% tune wq
+
+% tune wq
 figure(3)
 for w_q = 1:1:length(width_q_all)
     for i=1:length(t_gabor)
@@ -59,11 +60,11 @@ for w_q = 1:1:length(width_q_all)
     subplot(1, length(width_q_all), w_q)
     pcolor(t_gabor, ks/(2*pi), log(s_gabor.' + 1)), shading interp
     colormap('hot'), xlabel('Time [sec]'), ylabel('Frequency [Hz]')
-    axis([0, tr_gnr, 0, 2000])
+    axis([0, tr_gnr, 0, 1000])
     title(['wq = ', num2str(width_q_all(w_q))])
 end
 
-%% whole data
+% whole data
 [y, Fs] = audioread('GNR.m4a');             % read whole audio data
 tr_gnr = length(y)/Fs;              % record time in seconds
 L = tr_gnr;                         % time domin
@@ -77,6 +78,7 @@ ks = fftshift(k);
 num_gabor = 100;                            % number of the time points to take
 t_gabor = linspace(0, t(end), num_gabor);   % discretize the time
 s_gabor = zeros(length(t_gabor), n);        % matrix to store the Gabor transforms
+score = zeros(1, length(t_gabor));
 
 for i=1:length(t_gabor)
     gabor = exp(-100*(t - t_gabor(i)).^2);
@@ -84,6 +86,7 @@ for i=1:length(t_gabor)
     gyts = abs(fftshift(gyt));
     [val, ind] = max(gyts(n/2:end));
     [a,b] = ind2sub(size(gyts),ind+n/2-1);
+    score(i) = ks(b);
     s_filter = exp(-0.002 * ((ks - ks(b)).^2));
     gytf = fftshift(gyt).*s_filter;
     s_gabor(i,:) = abs(gytf);
@@ -98,6 +101,64 @@ title('spectrogram of GNR')
 
 %% task 1, Floyd
 clear; clc;
+close all
+[y, Fs] = audioread('Floyd.m4a');
+[y, Fs] = audioread('Floyd.m4a', [1, 3*Fs]);      % read first 3 seconds
+tr_floyd = length(y)/Fs;              % record time in seconds
+L = tr_floyd;                         % time domin
+n = length(y);                      % Fourier modes
+t1 = linspace(0, L, n + 1);
+t = t1(1:n);
+k = (2*pi/L)*[0:n/2-1, -n/2:-1];
+ks = fftshift(k);
+
+% create Gabor filter
+width_t_all = [10, 100, 1000, 10000];        % width of the filter
+width_q_all = [0.02, 0.002, 0.0002, 0.00002];           % width of the filter in frequency domain
+num_gabor = 100;                            % number of the time points to take
+t_gabor = linspace(0, t(end), num_gabor);   % discretize the time
+s_gabor = zeros(length(t_gabor), n);        % matrix to store the Gabor transforms
+
+% tune wt
+figure(4)
+for w_t = 1:1:length(width_t_all)
+    for i=1:length(t_gabor)
+        gabor = exp(-width_t_all(w_t)*(t - t_gabor(i)).^2);
+        gyt = fft(gabor.*y.');
+        gyts = abs(fftshift(gyt));
+        s_gabor(i,:) = gyts;
+    end
+
+    % plot the spectrogram
+    subplot(1, length(width_t_all), w_t)
+    pcolor(t_gabor, ks/(2*pi), log(s_gabor.' + 1)), shading interp
+    colormap('hot'), xlabel('Time [sec]'), ylabel('Frequency [Hz]')
+    axis([0, tr_floyd, 0, 1000])
+    title(['wt = ', num2str(width_t_all(w_t))])
+end
+
+%% tune wq
+figure(5)
+for w_q = 1:1:length(width_q_all)
+    for i=1:length(t_gabor)
+        gabor = exp(-100*(t - t_gabor(i)).^2);
+        gyt = fft(gabor.*y.');
+        gyts = abs(fftshift(gyt));
+        [val, ind] = max(gyts(n/2:end));
+        [a,b] = ind2sub(size(gyts),ind+n/2-1);
+        s_filter = exp(-width_q_all(w_q) * ((ks - ks(b)).^2));
+        gytf = fftshift(gyt).*s_filter;
+        s_gabor(i,:) = abs(gytf);
+    end
+
+    % plot the spectrogram
+    subplot(1, length(width_q_all), w_q)
+    pcolor(t_gabor, ks/(2*pi), log(s_gabor.' + 1)), shading interp
+    colormap('hot'), xlabel('Time [sec]'), ylabel('Frequency [Hz]')
+    axis([0, tr_floyd, 0, 1000])
+    title(['wq = ', num2str(width_q_all(w_q))])
+end
+%% all data
 [y_all, Fs] = audioread('Floyd.m4a');
 step = 6;                               % time step to cut the whole audio to slices
 num_gabor = 100;                        % number of the time points to take at each slice
@@ -106,17 +167,17 @@ n_all = length(y_all);
 L_all = length(y_all)/Fs;
 k_all = (2*pi/L_all)*[0:n_all/2-1, -n_all/2:-1];
 ks_all = fftshift(k_all);
-%%
-figure(3)
-for s_i=1:length(y_all)/(Fs*step)
+
+figure(6)
+for s_i=1:length(y_all)/(Fs*step)+1
     if s_i*step*Fs < length(y_all)
         [y, Fs] = audioread('Floyd.m4a', [(s_i-1)*step*Fs+1, s_i*step*Fs]);
     else
-        [y, Fs] = audioread('Floyd.m4a', [(s_i-1)*step*Fs+1, length(y_all)]);
+        [y, Fs] = audioread('Floyd.m4a', [(s_i-1)*step*Fs+1, length(y_all)-1]);
     end
     
-    tr_gnr = length(y)/Fs;                  % record time in seconds
-    L = tr_gnr;                             % time domin
+    tr_floyd = length(y)/Fs;                  % record time in seconds
+    L = tr_floyd;                             % time domin
     n = length(y);                          % Fourier modes
     t1 = linspace(0, L, n + 1);
     t = t1(1:n);
@@ -135,17 +196,18 @@ for s_i=1:length(y_all)/(Fs*step)
         gyts = abs(fftshift(gyt));
         [val, ind] = max(gyts(n/2:end));
         [a,b] = ind2sub(size(gyts),ind+n/2-1);
-        s_filter = exp(-0.05 * ((ks - ks(b)).^2));
+        s_filter = exp(-0.002 * ((ks - ks(b)).^2));
         gytf = fftshift(gyt).*s_filter;
         s_gabor(i,:) = abs(gytf);
     end
     
-    s_gabor_all((s_i-1)*num_gabor+1:s_i*num_gabor, (s_i-1)*step*Fs+1:s_i*step*Fs) = s_gabor;
+%     s_gabor_all((s_i-1)*num_gabor+1:s_i*num_gabor, (s_i-1)*step*Fs+1:s_i*step*Fs) = s_gabor;
     
-    pcolor(t_gabor, ks/(2*pi), log(s_gabor.' + 1)), shading interp
+    subplot(2,5,s_i)
+    pcolor(t_gabor + step*(s_i-1), ks/(2*pi), log(s_gabor.' + 1)), shading interp
     colormap('hot'), xlabel('Time [sec]'), ylabel('Frequency [Hz]')
-    axis([0, tr_gnr, 0, 1000])
-    title('Log of the spectrogram of GNR')
+    axis([step*(s_i-1), step*(s_i-1) + tr_floyd, 0, 1000])
+    title('Spectrogram of Floyd')
     drawnow
 end
 
