32529f76141a41002ce13e56cd28fdf9bbbd4f19
Guoqq17 <guoqq77@gmail.com>
2021-02-08 18:53:21 -0800

    spec_gnr

--- HW2/hw2.m
+++ HW2/hw2.m
@@ -8,8 +8,7 @@ title('Sweet Child O Mine');
 p8 = audioplayer(y,Fs); playblocking(p8);
 
 %% task 1, GNR
-[y_all, Fs] = audioread('GNR.m4a');     % read the audio data
-[y, Fs] = audioread('GNR.m4a', [1, 3*Fs]);
+[y, Fs] = audioread('GNR.m4a', [1, 3*Fs]);      % read first 3 seconds
 tr_gnr = length(y)/Fs;              % record time in seconds
 L = tr_gnr;                         % time domin
 n = length(y);                      % Fourier modes
@@ -64,10 +63,41 @@ for w_q = 1:1:length(width_q_all)
     title(['wq = ', num2str(width_q_all(w_q))])
 end
 
+%% whole data
+[y, Fs] = audioread('GNR.m4a');             % read whole audio data
+tr_gnr = length(y)/Fs;              % record time in seconds
+L = tr_gnr;                         % time domin
+n = length(y);                      % Fourier modes
+t1 = linspace(0, L, n + 1);
+t = t1(1:n);
+k = (2*pi/L)*[0:n/2-1, -n/2:-1];
+ks = fftshift(k);
+
+% create Gabor filter
+num_gabor = 100;                            % number of the time points to take
+t_gabor = linspace(0, t(end), num_gabor);   % discretize the time
+s_gabor = zeros(length(t_gabor), n);        % matrix to store the Gabor transforms
+
+for i=1:length(t_gabor)
+    gabor = exp(-100*(t - t_gabor(i)).^2);
+    gyt = fft(gabor.*y.');
+    gyts = abs(fftshift(gyt));
+    [val, ind] = max(gyts(n/2:end));
+    [a,b] = ind2sub(size(gyts),ind+n/2-1);
+    s_filter = exp(-0.002 * ((ks - ks(b)).^2));
+    gytf = fftshift(gyt).*s_filter;
+    s_gabor(i,:) = abs(gytf);
+end
+
+% plot the spectrogram
+figure(4)
+pcolor(t_gabor, ks/(2*pi), log(s_gabor.' + 1)), shading interp
+colormap('hot'), xlabel('Time [sec]'), ylabel('Frequency [Hz]')
+axis([0, tr_gnr, 0, 1000])
+title('spectrogram of GNR')
 
 %% task 1, Floyd
 clear; clc;
-
 [y_all, Fs] = audioread('Floyd.m4a');
 step = 6;                               % time step to cut the whole audio to slices
 num_gabor = 100;                        % number of the time points to take at each slice
